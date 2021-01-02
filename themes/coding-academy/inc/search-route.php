2e78e3f80e3c8e18c77d5d926dfd11edc69f9c93
Jjin SoL <parkleydondon@yahoo.com>
2021-01-02 10:05:54 -0800

    Combined front-end & back-end search function

--- themes/coding-academy/inc/search-route.php
+++ themes/coding-academy/inc/search-route.php
@@ -34,15 +34,24 @@ function academy_search_results($data) {
             ));    
         }
         if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
+            $desc = NULL;
+            if (has_excerpt()) { $desc =  get_the_excerpt(); }  
+            else { $desc = wp_trim_words(get_the_content(), 20); } 
+
             array_push($results['events'], array(
                 'title' => get_the_title(),
-                'permalink' => get_the_permalink()
+                'permalink' => get_the_permalink(),
+                'month' => $eventDate->format('M'),
+                'day' => $eventDate->format('d'),
+                'desc' => $desc
             ));    
         }
         if (get_post_type() == 'professor') {
             array_push($results['professors'], array(
                 'title' => get_the_title(),
-                'permalink' => get_the_permalink()
+                'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(0, 'professor-horizontal')
             ));    
         }
     }
